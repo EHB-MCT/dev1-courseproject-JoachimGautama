85f07eb728ea3176bc3463907e8e6b525f1b67d4
GJX_JVY <joachim.gautama@student.ehb.be>
2024-09-27 01:27:55 +0200

    finished sprite

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,25 @@
 "use strict";
 
-alert("Draw your space invader here");
+// alert("Draw your space invader here");
+
+function itsOneAndIHateMyself() {
+  let canvas = document.querySelector("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  let context = canvas.getContext("2d");
+
+  context.fillStyle = "black";
+  context.beginPath();
+  context.fillRect(50, 50, 300, 300);
+  context.closePath();
+
+  context.fillStyle = "#9AD37B";
+  context.fillRect(75, 75, 50, 100);
+  context.fillRect(175, 75, 50, 100);
+  context.fillRect(275, 75, 50, 100);
+  context.fillRect(125, 175, 150, 50);
+  context.fillRect(75, 225, 50, 50);
+  context.fillRect(275, 225, 50, 50);
+  context.fillRect(175, 275, 50, 50);
+}
+itsOneAndIHateMyself();
