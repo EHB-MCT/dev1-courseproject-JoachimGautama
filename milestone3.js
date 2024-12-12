c99319772e1fa7c1d613d4ba7153f8f4b74d412b
GJX_JVY <joachim.gautama@student.ehb.be>
2024-12-12 18:10:57 +0100

    cleanup

--- milestone3.js
+++ milestone3.js
@@ -12,31 +12,67 @@ let amount = 9;
 // for circles
 // circles in objects
 let collection = [];
-let funsies = 0;
 // distance between lines
 let dist = 180;
 // for perlin
+let perlinColours = [];
+let perlins = [];
 let optional = 0;
+//gradient
+let num = Math.random() * 80 + 120;
+const grd = context.createLinearGradient(0, num, 250, 0);
 
+// window.addEventListener("mousemove", )
+// to be finished with function to move perlin
+
+setup();
 drawImportant();
 
-function drawImportant() {
-  let num = Math.random() * 80 + 120;
+function setup() {
   // thx to w3schools for the knowledge on how to create a gradient (06/11/2024)
   // https://www.w3schools.com/tags/canvas_createlineargradient.asp#:~:text=The%20createLinearGradient()%20method%20creates,to%20strokeStyle%20or%20fillStyle%20properties.
-  const grd = context.createLinearGradient(0, num, 250, 0);
   grd.addColorStop(0, "rgb(131, 107,0)");
   grd.addColorStop(1, "rgb(116, 23, 179)");
   context.fillStyle = grd;
 
-  context.fillRect(0, 0, width + 300, height);
-  // to make wider windows look nice
-  if (width + 300 > 768) {
+  if (width + 300 >= 768) {
     amount += 3;
     dist = 250;
-    optional = 100;
+    optional = 95;
   }
+
+  for (let i = 0; i < amount; i++) {
+    // put in object collection[]
+    let valX;
+    if (i < amount / 2) {
+      valX = (width + 300) / 2;
+    } else {
+      valX = width + 150;
+    }
+    let circle = {
+      x: valX * Math.random() + 80,
+      y: (height - 200) * Math.random() + 100,
+    };
+    collection.push(circle);
+  }
+
+  for (let j = 0; j < 500; j++) {
+    let colour = {
+      r: Math.random() * 30,
+      g: 10,
+      b: 20,
+      a: Math.random() * 15 + 10,
+    };
+    perlinColours.push(colour);
+  }
+}
+
+function drawImportant() {
+  context.fillRect(0, 0, width + 300, height);
+  // to make wider windows look nice
+
   perlin();
+
   for (let i = 0; i < amount; i++) {
     let x = 150 + ((i + 1) / (amount + 1)) * width;
 
@@ -52,26 +88,6 @@ function drawImportant() {
     context.lineTo(x - dist + i * 45, height - 50);
     context.stroke();
 
-    let valX = 0;
-    if (i < amount / 2) {
-      valX = (width + 300) / 2;
-    } else {
-      valX = width + 150;
-    }
-
-    funsies = Math.ceil(Math.random() * 20);
-    circles(funsies, valX);
-  }
-}
-
-function circles(amount, max) {
-  for (let i = 0; i < amount; i++) {
-    // put in object collection[]
-    let circle = {
-      x: max * Math.random() + 80,
-      y: (height - 200) * Math.random() + 100,
-    };
-    collection.push(circle);
     Utils.fillCircle(collection[i].x, collection[i].y, 20 + Math.random() * 20);
   }
 }
@@ -79,21 +95,21 @@ function circles(amount, max) {
 function perlin() {
   // noise function info from https://www.geeksforgeeks.org/p5-js-noise-function/ (07/11/2024)
   // Using Peter Dickx's adaptation for the DEV1 course @ Erasmushogeschool Brussel
-  for (let j = 0; j < 400; j++) {
-    let r = Math.random() * 30;
-    let g = 10;
-    let b = 20;
-    let a = Math.random() * 15 + 10;
-    context.strokeStyle = Utils.rgba(r, g, b, a);
+  for (let j = 0; j < 500; j++) {
+    context.strokeStyle = Utils.rgba(
+      perlinColours[j].r,
+      perlinColours[j].g,
+      perlinColours[j].b,
+      perlinColours[j].a
+    );
     context.lineWidth = 5;
-
-    for (let i = 0; i <= height; i++) {
+    for (let i = 0; i <= height + 300; i++) {
       let x =
         i +
         (300 + width * j) / (130 + optional) +
         Noise.perlinNoise((i + 10 * j) / 300) * 200;
       let y = i;
-      context.strokeRect(x - 750, y, 1, 1);
+      context.strokeRect(x - 950, y, 1, 1);
     }
   }
 }
