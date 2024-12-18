603b302f203916595e41db4f4a43246a29930ac7
GJX_JVY <joachim.gautama@student.ehb.be>
2024-12-18 01:32:18 +0100

    before I make dumb mistakes

--- milestone3.js
+++ milestone3.js
@@ -18,25 +18,23 @@ let dist = 180;
 // for perlin
 let perlinColours = [];
 let perlins = [];
+let perlins2 = [];
 let optional = 0;
+let scale = 330;
 //gradient
 let num = Math.random() * 80 + 120;
 const grd = context.createLinearGradient(0, num, 250, 0);
 
-let invaderX = window.innerWidth - 200;
-let invaderY = window.innerHeight - 200;
-// window.addEventListener("mousemove", )
 // to be finished with function to move perlin
+let moveX = 0;
+let diffX = 0;
+let counter = 0;
+
+window.addEventListener("mousemove", mouse);
 
 setup();
 
 function setup() {
-  // thx to w3schools for the knowledge on how to create a gradient (06/11/2024)
-  // https://www.w3schools.com/tags/canvas_createlineargradient.asp#:~:text=The%20createLinearGradient()%20method%20creates,to%20strokeStyle%20or%20fillStyle%20properties.
-  grd.addColorStop(0, "rgb(131, 107,0)");
-  grd.addColorStop(1, "rgb(116, 23, 179)");
-  context.fillStyle = grd;
-
   if (width + 300 >= 768) {
     amount += 3;
     dist = 250;
@@ -47,7 +45,7 @@ function setup() {
     // lines
     let colour = {
       r: 100 + Math.random() * 155,
-      g: Math.random() * 155 + 100,
+      g: Math.random() * 155 + 150,
       b: Math.random() * 100,
       a: Math.random() * 40 + 15,
     };
@@ -67,6 +65,7 @@ function setup() {
     collection.push(circle);
   }
   // perlins
+  // colour
   for (let j = 0; j < 500; j++) {
     let colour = {
       r: Math.random() * 30,
@@ -76,32 +75,47 @@ function setup() {
     };
     perlinColours.push(colour);
   }
+  // creates line to be duplicated
+  for (let i = 0; i <= height + 300; i++) {
+    let perlin = {
+      x:
+        i +
+        (300 + width) / (130 + optional + i * 2) +
+        Noise.perlinNoise(i / 300) * scale -
+        15,
+      y: i,
+    };
+    perlins.push(perlin);
+    perlins2.push(perlin);
+  }
   drawImportant();
 }
 
 function drawImportant() {
+  // thx to w3schools for the knowledge on how to create a gradient (06/11/2024)
+  // https://www.w3schools.com/tags/canvas_createlineargradient.asp#:~:text=The%20createLinearGradient()%20method%20creates,to%20strokeStyle%20or%20fillStyle%20properties.
+  grd.addColorStop(0.2, "rgb(131, 107,0)");
+  grd.addColorStop(1, "rgb(118, 50, 121)");
+  context.fillStyle = grd;
   context.fillRect(0, 0, width + 300, height);
-  // to make wider windows look nice
 
   perlin();
 
   for (let i = 0; i < amount; i++) {
     let x = 150 + ((i + 1) / (amount + 1)) * width;
-
-    context.strokeStyle = Utils.rgba(
-      lineColour[i].r,
-      lineColour[i].g,
-      lineColour[i].b,
-      lineColour[i].a
-    );
-
+    // big lines
+    context.strokeStyle = "purple";
     context.lineWidth = 10;
     context.moveTo(x, y); //y is a global value declared at line 8
-    context.lineTo(x - dist + i * 45, height - 50);
+    context.lineTo(x - dist + i * 45, height - 45);
     context.stroke();
-
-    Utils.fillCircle(collection[i].x, collection[i].y, collection[i].size);
   }
+  console.log(lineColour);
+
+  balls();
+
+  signature();
+  // requestAnimationFrame(drawImportant);
 }
 
 function perlin() {
@@ -114,27 +128,65 @@ function perlin() {
       perlinColours[j].b,
       perlinColours[j].a
     );
+    let offsetX = width + 250 - j * 6;
+
     context.lineWidth = 3;
     for (let i = 0; i <= height + 300; i++) {
-      let x =
-        i +
-        (300 + width * j) / (130 + optional) +
-        Noise.perlinNoise((i + 10 * j) / 300) * 200;
-      let y = i;
-      context.strokeRect(x - 950, y, 1, 1);
+      let x = perlins[i].x + offsetX;
+      let y = perlins[i].y;
+      context.strokeRect(x, y, 1, 1);
+      perlins[i].x += diffX / 2;
+    }
+    counter += diffX;
+    if (counter >= 15 || counter <= -15) {
+      diffX *= -1;
     }
   }
-
-  signature();
 }
 
 function signature() {
-  context.fillStyle = "rgba(154, 211, 123, 0.36)";
-  context.fillRect(75 / 3 + invaderX, 75 / 3 + invaderY, 50 / 3, 100 / 3);
-  context.fillRect(175 / 3 + invaderX, 75 / 3 + invaderY, 50 / 3, 100 / 3);
-  context.fillRect(275 / 3 + invaderX, 75 / 3 + invaderY, 50 / 3, 100 / 3);
-  context.fillRect(125 / 3 + invaderX, 175 / 3 + invaderY, 150 / 3, 50 / 3);
-  context.fillRect(75 / 3 + invaderX, 225 / 3 + invaderY, 50 / 3, 50 / 3);
-  context.fillRect(275 / 3 + invaderX, 225 / 3 + invaderY, 50 / 3, 50 / 3);
-  context.fillRect(175 / 3 + invaderX, 275 / 3 + invaderY, 50 / 3, 50 / 3);
+  let o = 5;
+  let s = 110;
+  let invaderX = window.innerWidth - s;
+  let invaderY = window.innerHeight - s;
+  context.fillStyle = "rgba(154, 211, 123, 0.5)";
+  context.fillRect(75 / o + invaderX, 75 / o + invaderY, 50 / o, 100 / o);
+  context.fillRect(175 / o + invaderX, 75 / o + invaderY, 50 / o, 100 / o);
+  context.fillRect(275 / o + invaderX, 75 / o + invaderY, 50 / o, 100 / o);
+  context.fillRect(125 / o + invaderX, 175 / o + invaderY, 150 / o, 50 / o);
+  context.fillRect(75 / o + invaderX, 225 / o + invaderY, 50 / o, 50 / o);
+  context.fillRect(275 / o + invaderX, 225 / o + invaderY, 50 / o, 50 / o);
+  context.fillRect(175 / o + invaderX, 275 / o + invaderY, 50 / o, 50 / o);
+}
+
+function balls() {
+  for (let i = 0; i < amount; i++) {
+    // continue
+    context.fillStyle = Utils.rgba(
+      lineColour[i].r,
+      lineColour[i].g,
+      lineColour[i].b,
+      lineColour[i].a
+    );
+    let x = collection[i].x;
+    let y = collection[i].y;
+
+    Utils.fillCircle(x, y, collection[i].size);
+  }
+}
+
+/**
+ * @param {mouseEvent} e
+ */
+
+function mouse(e) {
+  let mouseX = e.pageX;
+  // let mouseY = e.pageY;
+
+  if (moveX - mouseX > 0) {
+    diffX = -1;
+  } else if (moveX - mouseX < 0) {
+    diffX = +1;
+  }
+  moveX = mouseX;
 }
